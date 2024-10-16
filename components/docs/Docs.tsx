b6948d8e198f17273f37dd2511e89a83b05c7ab2
legiononene <116795291+exploresahil@users.noreply.github.com>
2024-10-16 18:04:51 +0530

    updated code

--- components/docs/Docs.tsx
+++ components/docs/Docs.tsx
@@ -2,14 +2,13 @@ import Code from "@/context/code/Code";
 import { MarkdownFile } from "@/types/types";
 import Markdown from "markdown-to-jsx";
 import "./style.scss";
-import { RefreshCw } from "lucide-react";
 
 const Docs = ({ data }: { data: MarkdownFile }) => {
   return (
     <div id="Docs">
       <div className="date_time">
-        <p>Created At: {new Date(data.createdAt).toLocaleString()}</p>
-        <p>Updated At: {new Date(data.updatedAt).toLocaleString()}</p>
+        <p>Created At: {new Date(data.createdAt).toLocaleDateString()}</p>
+        <p>Updated At: {new Date(data.updatedAt).toLocaleDateString()}</p>
       </div>
       <Markdown
         className="md"
